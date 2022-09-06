4d6ecdd1c9220370f63f76bc8c4f36d27c52d49d
motttey <motitago@gmail.com>
2022-09-06 20:47:56 +0900

    fix: add c101 pages

---
+++ pages/c101/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-layout
+      column
+      justify-center
+      align-center
+    >
+      <v-flex
+        xs12
+        sm9
+        md9
+      >
+        <v-container fluid>
+            <section>
+                Section 1
+            </section>
+            <section>
+                Section 2
+            </section>
+        </v-container>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+    name: "C101",
+}
+
+</script>
+
+<style scoped>
+section {
+    height: 100vh;
+}
+</style>
