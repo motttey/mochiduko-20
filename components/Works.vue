00c0b555b0ff317c55f1b5880306a1600e86edbd
motttey <motitago@gmail.com>
2022-12-06 21:07:47 +0900

    fix: self-introduce

--- components/Works.vue
+++ components/Works.vue
@@ -59,7 +59,7 @@
                <template v-slot:placeholder>
                  <v-row
                    class="fill-height ma-0"
-                   align="center"
+                   align-center
                    justify="center"
                  >
                    <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
