d10913ae6b179afb8158dee5a8dd5355fdd50c69
motttey <motitago@gmail.com>
2023-01-26 08:54:44 +0900

    modify: devide test directory

--- test-playwright/index.spec.js
+++ test-playwright/index.spec.js
@@ -14,7 +14,7 @@ test('screenshot', async () => {
   const browser = await webkit.launch()
   const context = await browser.newContext()
   const page = await context.newPage()
-  await page.goto('http://localhost:3000/')
+  await page.goto(mochidukoUrl)
 
   // Expect a title "to contain" a substring.
   expect(await page.screenshot({ fullPage: true })).toMatchSnapshot()
