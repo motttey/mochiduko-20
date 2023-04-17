e86695def965c57c8257a582ae955d2ec09d60a1
motttey <motitago@gmail.com>
2023-04-17 21:45:16 +0900

    fix: wait until recovery

--- middleware/bff.js
+++ middleware/bff.js
@@ -1,16 +1,19 @@
 export default function ({ $axios, store }) {
     const MAX_API_ACCESS_PER_USER = 3; // ユーザごとのAPIアクセス回数の上限
+    // 10秒待機
+    const WAIT_TIME_RETRIES = 10000;
 
     $axios.onRequest(config => {
         console.log('Making request to ', config.url);
         // ユーザごとのAPIアクセス回数をカウントする
-        if (!store.state.user.apiAccessCount) {
-            store.commit('resetApiAccessCount')
-        }
         store.commit('incrementUserCount')
         // ユーザのAPIアクセス回数が上限に達した場合はエラーを返す
         if (store.state.user.apiAccessCount > MAX_API_ACCESS_PER_USER) {
             console.log('アクセス制限を超えました');
+
+            setInterval(() => {
+                store.commit('resetApiAccessCount')
+            }, WAIT_TIME_RETRIES);
             return Promise.reject(new Error('APIへのアクセス制限回数を超えました'));
         }
     });
