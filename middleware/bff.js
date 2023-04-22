7968d8515f993c7d45c7e18a74f61cca132827e8
motttey <motitago@gmail.com>
2023-04-22 16:20:31 +0900

    fix: add condition isDev

--- middleware/bff.js
+++ middleware/bff.js
@@ -1,11 +1,13 @@
-export default function ({ $axios, $auth, store }) {
+export default function ({ $axios, isDev, store }) {
     const MAX_API_ACCESS_PER_USER = 3; // ユーザごとのAPIアクセス回数の上限
     // 10秒待機
     const WAIT_TIME_RETRIES = 10000;
     
     $axios.onRequest(config => {
         console.log('Making request to ', config.url);
-
+        
+        // dev環境の時はそのまま
+        if (isDev) return;
         // ユーザのAPIアクセス回数が上限に達した場合はエラーを返す
         if (
             store.state.accessCountMap[store.state.user.userId] 
