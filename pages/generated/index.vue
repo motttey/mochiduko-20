33ef6b18a83d65cd0ed84ca0f28821fab3dc95ad
motttey <motitago@gmail.com>
2023-07-25 21:20:03 +0900

    modify: layout of /generated

--- pages/generated/index.vue
+++ pages/generated/index.vue
@@ -12,13 +12,22 @@
       <v-container fluid>
         <v-row>
           <v-col cols="12" sm="6" offset-sm="3">
+            <h1 
+              class="header"
+              style="text-align: center;"
+            >
+              AIが描いたドラえもん
+            </h1>
+            <p class="caption">
+              Genetated by
+              <a href="https://stablediffusionapi.com/">Stable Diffusion API</a>
+            </p>
             <v-card flat>
-              <h1>AIが描いたドラえもん</h1>
               <v-img
                 v-if="images.length > 0"
                 :src="api_url + getRandomImg(images).image"
                 :lazy-src="api_url + getRandomImg(images).image"
-                alt="generated doraemon"
+                alt="Generated Doraemon Image"
                 aspect-ratio="auto"
                 max-height="600px"
                 class="grey lighten-2"
@@ -54,7 +63,6 @@ export default {
   // asyncDataに書き直す
   methods: {
     getRandomImg(images) {
-      console.log(images[Math.floor(Math.random()*images.length)]);
       return images[Math.floor(Math.random()*images.length)]
     },
     async getImage() {
