77761b319e0c5cb688ce8166e1e20ad582500a30
motttey <motitago@gmail.com>
2025-10-13 19:33:49 +0900

    uninstall: mdi

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,7 +36,6 @@ export default defineNuxtConfig({
   },
   css: [
     'vuetify/lib/styles/main.sass',
-    '@mdi/font/css/materialdesignicons.min.css',
     '@fortawesome/fontawesome-svg-core/styles.css',
   ],
   modules: [
